34c9c87fe9864c449de987519dae4d1f995e47f4
Hemanth Nadikota <nadikota.chandra@ril.com>
2025-06-08 13:24:51 +0530

    add feed path

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
             <Route path="/profile" element={<Profile />} />
             <Route path="/connections" element={<Connections />} />
             <Route path="/requests" element={<Requests />} />
+            <Route path="/" element={<Feed />} />
           </Route>
         </Routes>
       </Router>
